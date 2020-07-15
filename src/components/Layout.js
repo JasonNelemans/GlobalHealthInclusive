8e36aed6f99b6f01e86a496062876fd473a9c415
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-15 22:48:22 +0200

    created Layout component

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+// Components
+import Header from "./Header"
+import Footer from "./Footer"
+
+const Layout = props => {
+  return (
+    <div>
+      <Header />
+      {props.children}
+      <Footer />
+    </div>
+  )
+}
