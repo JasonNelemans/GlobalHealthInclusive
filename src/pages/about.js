3ec1cc286332c6da9e5387adb4075b2f4dfa86de
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-22 15:55:30 +0200

    rendered about data and applied simple styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,9 +7,91 @@ import styled from "styled-components"
 import Layout from "../components/Layout"
 
 export default function About({ data }) {
+  console.log("data: ", data)
   return (
     <Layout>
-      <h1>This is the about page.</h1>
+      <AboutContainer>
+        <div className="margin-maker">
+          <div className="info-container">
+            <div className="about-text">
+              <h3>
+                {data.allContentfulAbout.edges[0].node.aboutContent.title}
+              </h3>
+              {documentToReactComponents(
+                data.allContentfulAbout.edges[0].node.aboutContent.content.json
+              )}
+            </div>
+          </div>
+          <div className="about-divider" />
+        </div>
+      </AboutContainer>
     </Layout>
   )
 }
+
+const AboutContainer = styled.div`
+  margin: 35px auto 70px;
+
+  .margin-maker {
+    margin: 10px;
+  }
+
+  .info-container {
+    display: flex;
+    justify-content: center;
+  }
+
+  .about-text {
+    max-width: 1000px;
+  }
+
+  .about-divider {
+    margin: 25px auto 0;
+    border-top: 1px solid #31a069;
+    max-width: 1000px;
+  }
+
+  h3 {
+    font-size: 30px;
+    color: #31419d;
+    letter-spacing: 1.5px;
+  }
+
+  p {
+    color: #a1a5a9;
+  }
+
+  b {
+    color: black;
+  }
+`
+
+export const data = graphql`
+  query {
+    allContentfulAbout {
+      edges {
+        node {
+          aboutContent {
+            title
+            content {
+              json
+            }
+          }
+          testimonials {
+            name
+            credentials
+            image {
+              title
+              file {
+                url
+              }
+            }
+            content {
+              json
+            }
+          }
+        }
+      }
+    }
+  }
+`
