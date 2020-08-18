2a7c6c6835b706064f365a67618ea216ea5545c5
JasonNelemans <jasonnelemans@hotmail.com>
2020-08-18 15:56:39 +0200

    render information data dynamically in template

--- src/templates/information.js
+++ src/templates/information.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import styled from "styled-components"
 
 import Layout from "../components/Layout"
 import Head from "../components/Head"
@@ -10,6 +11,9 @@ export const query = graphql`
     contentfulInformationPosts(slug: { eq: $slug }) {
       title
       slug
+      header {
+        json
+      }
       content {
         json
       }
@@ -20,10 +24,48 @@ export const query = graphql`
 export default function Information(props) {
   return (
     <Layout>
-      <h1>{props.data.contentfulInformationPosts.title}</h1>
-      {documentToReactComponents(
-        props.data.contentfulInformationPosts.content.json
-      )}
+      <Head title={props.data.contentfulInformationPosts.title} />
+      <InformationContainer>
+        <InformationText>
+          <ReturnButton>Return</ReturnButton>
+          <div className="information-header">
+            <em>
+              {documentToReactComponents(
+                props.data.contentfulInformationPosts.header.json
+              )}
+            </em>
+          </div>
+          {documentToReactComponents(
+            props.data.contentfulInformationPosts.content.json
+          )}
+        </InformationText>
+      </InformationContainer>
     </Layout>
   )
 }
+
+const InformationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 50px;
+`
+const InformationText = styled.div`
+  margin: 15px;
+  max-width: 900px;
+  line-height: 1.5;
+
+  .information-header {
+    margin-bottom: 30px;
+  }
+`
+
+const ReturnButton = styled.div`
+  border: 2px solid #219cbd;
+  color: #fff;
+  border-radius: 5px;
+  display: inline-block;
+  padding: 3px;
+  background-color: #219cbd;
+`
