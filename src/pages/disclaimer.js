2d8f729e03e5e419d9617af427e72b1f925fe2ee
JasonNelemans <jasonnelemans@hotmail.com>
2020-08-19 20:10:32 +0200

    temporary white paged other pages too

--- src/pages/disclaimer.js
+++ src/pages/disclaimer.js
@@ -1,71 +1,71 @@
-import React from "react"
-import { graphql } from "gatsby"
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
-import styled from "styled-components"
+// import React from "react"
+// import { graphql } from "gatsby"
+// import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+// import styled from "styled-components"
 
-// Components
-import Layout from "../components/Layout"
-import Head from "../components/Head"
+// // Components
+// import Layout from "../components/Layout"
+// import Head from "../components/Head"
 
-export default function Disclaimer({ data }) {
-  return (
-    <Layout>
-      <Head title="Disclaimer" />
-      <DisclaimerContainer>
-        <div className="disclaimer-text">
-          {documentToReactComponents(
-            data.allContentfulDisclaimerPage.edges[0].node.content.json
-          )}
-        </div>
-      </DisclaimerContainer>
-    </Layout>
-  )
-}
+// export default function Disclaimer({ data }) {
+//   return (
+//     <Layout>
+//       <Head title="Disclaimer" />
+//       <DisclaimerContainer>
+//         <div className="disclaimer-text">
+//           {documentToReactComponents(
+//             data.allContentfulDisclaimerPage.edges[0].node.content.json
+//           )}
+//         </div>
+//       </DisclaimerContainer>
+//     </Layout>
+//   )
+// }
 
-const DisclaimerContainer = styled.div`
-  background-color: #f7f7f7;
-  line-height: 1.85714285714286;
-  padding: 50px 50px 65px;
+// const DisclaimerContainer = styled.div`
+//   background-color: #f7f7f7;
+//   line-height: 1.85714285714286;
+//   padding: 50px 50px 65px;
 
-  .disclaimer-text {
-    padding: 3em 6.67em;
-    background-color: white;
-    max-width: 900px;
-    margin: 0 auto;
+//   .disclaimer-text {
+//     padding: 3em 6.67em;
+//     background-color: white;
+//     max-width: 900px;
+//     margin: 0 auto;
 
-    @media (max-width: 900px) {
-      padding: 20px;
-    }
-  }
+//     @media (max-width: 900px) {
+//       padding: 20px;
+//     }
+//   }
 
-  h1 {
-    color: #31419d;
-  }
+//   h1 {
+//     color: #31419d;
+//   }
 
-  p {
-    color: #a1a5a9;
-  }
+//   p {
+//     color: #a1a5a9;
+//   }
 
-  b {
-    color: black;
-  }
+//   b {
+//     color: black;
+//   }
 
-  @media (max-width: 900px) {
-    padding: 40px;
-  }
-`
+//   @media (max-width: 900px) {
+//     padding: 40px;
+//   }
+// `
 
-export const data = graphql`
-  query {
-    allContentfulDisclaimerPage {
-      edges {
-        node {
-          title
-          content {
-            json
-          }
-        }
-      }
-    }
-  }
-`
+// export const data = graphql`
+//   query {
+//     allContentfulDisclaimerPage {
+//       edges {
+//         node {
+//           title
+//           content {
+//             json
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
